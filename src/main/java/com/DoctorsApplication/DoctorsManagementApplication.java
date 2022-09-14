0991110b46f95a7e9a183a6ab3db1f42024ef407
Sajith <Sajith.S@mindtree.com>
2022-09-14 11:47:05 +0530

    First Commoit on docter management spring boot project

---
+++ src/main/java/com/DoctorsApplication/DoctorsManagementApplication.java
@@ -0,0 +1,13 @@
+package com.DoctorsApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/DoctorsApplication/DoctorsManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.DoctorsApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctorsManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
